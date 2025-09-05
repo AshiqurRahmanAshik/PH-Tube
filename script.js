8162061eefafb44822a53fbe57938a380558d748
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-09-05 17:43:26 +0600

    Some Modifications Tried but do not work

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const categoryContainer = document.getElementById("categoryContainer");
+const VideoContainer = document.getElementById("videoContainer");
 
 const loadCategory = () => {
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
@@ -13,19 +14,48 @@ const displayCategory = (categories) => {
   });
   categoryContainer.addEventListener("click", (e) => {
     if (e.target.tagName === "BUTTON") {
+      console.log(e.target.innerText);
       removeBtnColor();
       const button = e.target;
-      console.log(button);
+      //console.log(button);
       button.classList.add("bg-red-500", "text-white");
     }
   });
 };
-
+const loadVideo = () => {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data) => displayVideo(data.videos));
+};
+const displayVideo = (videos) => {
+  //console.log(videos);
+  videos.forEach((video) => {
+    videoContainer.innerHTML += `
+     <div class="relative">
+     <img src="${video.thumbnail}" alt="thumbnail" class="w-full h-48 object-cover rounded-sm" />
+    
+        <div class="flex mt-2 gap-2">
+          <img class="w-10 h-10 rounded-full" src="${video.authors[0].profile_picture}" alt="logo" />
+          <div>
+            <p class="font-bold text-xl" >${video.title}</p>
+            <p>
+              ${video.authors[0].profile_name}"<i
+                class="fa-solid fa-circle-check"
+                style="color: #3b2eea"
+              ></i>
+            </p>
+            <p>${video.others.views} views</p>
+          </div>
+        </div>
+      </div>`;
+  });
+};
 loadCategory();
+loadVideo();
 
 const removeBtnColor = () => {
   const allBtn = document.querySelectorAll(".category-btn");
-  console.log(allBtn);
+  //console.log(allBtn);
   allBtn.forEach((btn) => {
     btn.classList.remove("bg-red-500", "text-white");
   });
